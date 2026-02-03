077f68764bcb2116a16e1a638473d14ab6a15555
samsoule <samuelmartinezpiedra22@gmail.com>
2026-02-03 15:46:09 +0100

    Delete jeu directory

--- jeu/menu.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll("[data-level]").forEach(btn => {
-  btn.addEventListener("click", () => {
-    const target = btn.getAttribute("data-level");
-    if (target) window.location.href = target;
-  });
-});
